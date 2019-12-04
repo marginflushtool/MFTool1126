f7c80f8d624ee6884c76571c9193f89e0342665a
TSEFTON <tsefton@ford.com>
2019-12-04 15:59:00 -0500

    Working app with distance measurement of houglines displayed to screen

--- app/src/main/java/com/example/mftool1126/MainActivity.java
+++ app/src/main/java/com/example/mftool1126/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mftool1126;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +16,14 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.w3c.dom.Text;
 
 
 import java.util.Random;
@@ -30,8 +35,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
     boolean startCanny = false;
+    double lineDist = 2.0;
 
 
+//    boolean collectFrames = false;
+//    Mat measuredImage = new Mat();
+ //   ImageView image = (ImageView) findViewById(R.id.CameraView);
 
 
 
@@ -40,20 +49,25 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         if (startCanny == false){
             startCanny = true;
+
         }
 
         else{
 
             startCanny = false;
 
+
+
+         /*   cameraBridgeViewBase.disableView();
+            Bitmap bitmap1 = Bitmap.createBitmap(measuredImage.cols(), measuredImage.rows(),Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(measuredImage, bitmap1 );
+            image.setImageBitmap(bitmap1);
+*/
+
         }
 
 
     }
-
-
-
-
 
 
     @Override
@@ -77,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
                     case BaseLoaderCallback.SUCCESS:
                         cameraBridgeViewBase.enableView();
+
                         break;
                     default:
                         super.onManagerConnected(status);
@@ -94,44 +109,54 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
-        Mat frame = inputFrame.rgba();
-        Mat lines = new Mat();
+            Mat frame = inputFrame.rgba();
+            Mat imagCaputred = new Mat();
+            Mat lines = new Mat();
+            imagCaputred = frame;
 
-        if (startCanny == true) {
-
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
-
-            Imgproc.Canny(frame, frame, 100, 75);
-
-            Imgproc.HoughLinesP (frame, lines, 1, Math.PI / 180, 100, 50, 50);
-
-           for(int i = 0; i < lines.cols(); i++) {
-
-               double[] vec = lines.get(0, i);
-               double x1 = vec[0],
-                       y1 = vec[1],
-                       x2 = vec[2],
-                       y2 = vec[3];
-
-               Point start = new Point(x1, y1);
-               Point end = new Point(x2, y2);
-
-               Imgproc.line(frame, start, end, new Scalar(255, 255, 255), 5);
-
-               double dx = x2 - x1;
-               double dy = y2 - y1;
+        if (startCanny == true ) {
 
 
+                Imgproc.cvtColor(imagCaputred, imagCaputred, Imgproc.COLOR_RGBA2GRAY);
 
-/*               String ldText = Double.toString(dx); ?? move distance to screen
-*/
+                Imgproc.Canny(imagCaputred, imagCaputred, 100, 75);
+
+                Imgproc.HoughLinesP(imagCaputred, lines, 1, Math.PI / 180, 100, 50, 50);
+
+                for (int i = 0; i < lines.cols(); i++) {
+
+                    double[] vec = lines.get(0, i);
+                    double x1 = vec[0],
+                            y1 = vec[1],
+                            x2 = vec[2],
+                            y2 = vec[3];
+
+                    Point start = new Point(x1, y1);
+                    Point end = new Point(x2, y2);
+
+                    Imgproc.line(frame, start, end, new Scalar(255, 255, 255), 5);
+
+                    double dx = x2 - x1;
+                    double dy = y2 - y1;
+                    lineDist = dx;
+
+
+
+
+                /*               String ldText = Double.toString(dx); ?? move distance to screen
+                     */
+                }
+
+            final TextView mainview = (TextView)findViewById(R.id.textView2);
+            mainview.setText(String.valueOf(lineDist));
+
+
 
             }
+ //           measuredImage = imagCaputred;
 
+        return imagCaputred;
 
-        }
-
-        return frame;
     }
 
 
